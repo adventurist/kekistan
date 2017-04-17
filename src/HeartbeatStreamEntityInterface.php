dac0c15db3c3cc0918370bc9c28f26286b1527d9
logicp <emmanuel.buckshi@opin.ca>
2017-04-17 16:44:58 -0400

    Adding getters and setters with phpdocs

--- src/HeartbeatStreamEntityInterface.php
+++ src/HeartbeatStreamEntityInterface.php
@@ -9,4 +9,69 @@ use Drupal\Core\Config\Entity\ConfigEntityInterface;
  */
 interface HeartbeatStreamEntityInterface extends ConfigEntityInterface {
   // Add get/set methods for your configuration properties here.
+
+
+  /*
+   * Sets the unique Message ID
+   *
+   * @param string $messageId
+   *  The unique Message ID to represent
+   *  all messages of this type
+   */
+
+  public function setMessageId($messageId);
+
+  /*
+   * Gets the unique Message ID
+   *
+   * @return string
+   *  The Stream's Message ID
+   */
+
+  public function getMessageId();
+
+
+  /**
+   * Sets the description of the stream
+   *
+   * @param string $description
+   *  Describing streams of this type
+   */
+
+  public function setDescription($description);
+
+  /**
+   * Gets the description of the stream
+   *
+   * @return string
+   *  The Stream's description
+   */
+  public function getDescription();
+
+
+  /**
+   * Sets the translatable message
+   * This message creates the structure of each message
+   *
+   * @param string $message
+   *  The template message serving as the foundation of each message structure of this stream type
+   */
+
+  public function setMessage();
+
+  public function getMessage();
+
+  public function setMessageConcat();
+
+  public function getMessageConcat();
+
+  public function setPerms();
+
+  public function getPerms();
+
+  public function setGroupType();
+
+  public function getGroupType();
+
+
 }
