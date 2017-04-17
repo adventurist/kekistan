f39f9d059d2cf51ae87bbb2c9e03da3a4b0a7e01
logicp <emmanuel.buckshi@opin.ca>
2017-04-17 17:08:20 -0400

    Added remaining streamConfiguration methods methods overridden in HeartbeatStreamEntity which implements that interface

--- src/Entity/HeartbeatStreamEntity.php
+++ src/Entity/HeartbeatStreamEntity.php
@@ -53,4 +53,176 @@ class HeartbeatStreamEntity extends ConfigEntityBase implements HeartbeatStreamE
    */
   protected $label;
 
+
+
+  
+
+  public function setMessageId($messageId) {
+    // TODO: Implement setMessageId() method.
+  }
+
+  public function getMessageId() {
+    // TODO: Implement getMessageId() method.
+  }
+
+  /**
+   * Sets the description of the stream
+   *
+   * @param string $description
+   *  Describing streams of this type
+   */
+  public function setDescription($description) {
+    // TODO: Implement setDescription() method.
+  }
+
+  /**
+   * Gets the description of the stream
+   *
+   * @return string
+   *  The Stream's description
+   */
+  public function getDescription() {
+    // TODO: Implement getDescription() method.
+  }
+
+  /**
+   * Sets the translatable message
+   * This message creates the structure of each message
+   *
+   * @param string $message
+   *  The template message serving as the foundation of each message structure of this stream type
+   */
+  public function setMessage($message) {
+    // TODO: Implement setMessage() method.
+  }
+
+  /**
+   * Gets the translatable message of the stream
+   *
+   * @return string
+   *  The Stream's message
+   */
+  public function getMessage() {
+    // TODO: Implement getMessage() method.
+  }
+
+  /**
+   * Sets the translatable concatenated message
+   *
+   * @param string $messageConcat
+   *
+   */
+  public function setMessageConcat($messageConcat) {
+    // TODO: Implement setMessageConcat() method.
+  }
+
+  /**
+   * Gets the concatenated message of the stream
+   *
+   * @return string
+   *  The Stream's concatenated message
+   */
+  public function getMessageConcat() {
+    // TODO: Implement getMessageConcat() method.
+  }
+
+  /**
+   * Sets the Permissions for this message stream
+   *
+   * @param int $perms
+   *
+   */
+  public function setPerms($perms) {
+    // TODO: Implement setPerms() method.
+  }
+
+  /**
+   * Gets the Permissions of this message stream
+   *
+   * @return int
+   *  The stream's permissions
+   */
+  public function getPerms() {
+    // TODO: Implement getPerms() method.
+  }
+
+  /**
+   * Sets the Group Type for this message stream
+   *
+   * @param string $groupType
+   *
+   */
+  public function setGroupType($groupType) {
+    // TODO: Implement setGroupType() method.
+  }
+
+  /**
+   * Gets the Group Type of this message stream
+   *
+   * @return string
+   *  The stream's Group Type
+   */
+  public function getGroupType() {
+    // TODO: Implement getGroupType() method.
+  }
+
+  /**
+   * Sets the arguments for the concatenated message
+   *
+   * @param string $concatArgs
+   *
+   */
+  public function setConcatArgs($concatArgs) {
+    // TODO: Implement setConcatArgs() method.
+  }
+
+  /**
+   * Gets the arguments for the concatenated message
+   *
+   * @return string
+   *  The stream's arguments for the concatenated message
+   */
+  public function getConcateArgs() {
+    // TODO: Implement getConcateArgs() method.
+  }
+
+  /**
+   * Sets the variables for this message stream
+   *
+   * @param string $variables
+   *
+   */
+  public function setVariables($variables) {
+    // TODO: Implement setVariables() method.
+  }
+
+  /**
+   * Gets the variables of this message stream
+   *
+   * @return string
+   *  The stream's variables
+   */
+  public function getVariables() {
+    // TODO: Implement getVariables() method.
+  }
+
+  /**
+   * Sets the attachments for this message stream
+   *
+   * @param string $attachments
+   *
+   */
+  public function setAttachments($attachments) {
+    // TODO: Implement setAttachments() method.
+  }
+
+  /**
+   * Gets the attachments of this message stream
+   *
+   * @return string
+   *  The stream's attachments
+   */
+  public function getAttachments() {
+    // TODO: Implement getAttachments() method.
+  }
 }
--- src/HeartbeatStreamEntityInterface.php
+++ src/HeartbeatStreamEntityInterface.php
@@ -48,7 +48,6 @@ interface HeartbeatStreamEntityInterface extends ConfigEntityInterface {
    */
   public function getDescription();
 
-
   /**
    * Sets the translatable message
    * This message creates the structure of each message
@@ -57,21 +56,138 @@ interface HeartbeatStreamEntityInterface extends ConfigEntityInterface {
    *  The template message serving as the foundation of each message structure of this stream type
    */
 
-  public function setMessage();
+  public function setMessage($message);
+
+  /**
+   * Gets the translatable message of the stream
+   *
+   * @return string
+   *  The Stream's message
+   */
 
   public function getMessage();
 
-  public function setMessageConcat();
+
+  /**
+   * Sets the translatable concatenated message
+   *
+   * @param string $messageConcat
+   *
+   */
+
+  public function setMessageConcat($messageConcat);
+
+
+  /**
+   * Gets the concatenated message of the stream
+   *
+   * @return string
+   *  The Stream's concatenated message
+   */
 
   public function getMessageConcat();
 
-  public function setPerms();
+
+  /**
+   * Sets the Permissions for this message stream
+   *
+   * @param int $perms
+   *
+   */
+
+  public function setPerms($perms);
+
+
+  /**
+   * Gets the Permissions of this message stream
+   *
+   * @return int
+   *  The stream's permissions
+   */
 
   public function getPerms();
 
-  public function setGroupType();
+
+  /**
+   * Sets the Group Type for this message stream
+   *
+   * @param string $groupType
+   *
+   */
+
+  public function setGroupType($groupType);
+
+
+  /**
+   * Gets the Group Type of this message stream
+   *
+   * @return string
+   *  The stream's Group Type
+   */
 
   public function getGroupType();
 
+
+  /**
+   * Sets the arguments for the concatenated message
+   *
+   * @param string $concatArgs
+   *
+   */
+
+
+  public function setConcatArgs($concatArgs);
+
+
+  /**
+   * Gets the arguments for the concatenated message
+   *
+   * @return string
+   *  The stream's arguments for the concatenated message
+   */
+
+  public function getConcateArgs();
+
+
+
+  /**
+   * Sets the variables for this message stream
+   *
+   * @param string $variables
+   *
+   */
+
+  public function setVariables($variables);
+
+
+  /**
+   * Gets the variables of this message stream
+   *
+   * @return string
+   *  The stream's variables
+   */
+
+  public function getVariables();
+
+
+
+  /**
+   * Sets the attachments for this message stream
+   *
+   * @param string $attachments
+   *
+   */
+
+  public function setAttachments($attachments);
+
+
+  /**
+   * Gets the attachments of this message stream
+   *
+   * @return string
+   *  The stream's attachments
+   */
+
+  public function getAttachments();
 
 }
