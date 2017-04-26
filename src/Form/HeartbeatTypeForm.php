804e0cd98fd5892f99ce3105f5365e50a829b5e3
logicp <emmanuel@openconcept.ca>
2017-04-25 23:19:50 -0400

    HeartbeatTypeForm: debugging / improvements removed cruft added CSS library added better token tree

--- src/Form/HeartbeatTypeForm.php
+++ src/Form/HeartbeatTypeForm.php
@@ -2,20 +2,63 @@
 
 namespace Drupal\heartbeat8\Form;
 
+use Drupal\Core\Render\Renderer;
 use Drupal\heartbeat8;
 use Drupal\Core\Ajax\AjaxResponse;
 use Drupal\Core\Ajax\HtmlCommand;
 use Drupal\Core\Ajax\InvokeCommand;
 use Drupal\Core\Entity\EntityForm;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\token\TreeBuilder;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Class HeartbeatTypeForm.
  *
+ * @property \Drupal\Component\Render\MarkupInterface|string tokenTree
  * @package Drupal\heartbeat8\Form
  */
-class HeartbeatTypeForm extends EntityForm
-{
+class HeartbeatTypeForm extends EntityForm {
+
+  protected $treeBuilder;
+
+  protected $renderer;
+
+  private $tokenTree;
+
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static($container->get('token.tree_builder'), $container->get('renderer'));
+  }
+
+
+  /**
+   * PHP 5 allows developers to declare constructor methods for classes.
+   * Classes which have a constructor method call this method on each newly-created object,
+   * so it is suitable for any initialization that the object may need before it is used.
+   *
+   * Note: Parent constructors are not called implicitly if the child class defines a constructor.
+   * In order to run a parent constructor, a call to parent::__construct() within the child constructor is required.
+   *
+   * param [ mixed $args [, $... ]]
+   * @param TreeBuilder $tree_builder
+   * @param Renderer $renderer
+   * @link http://php.net/manual/en/language.oop5.decon.php
+   * @throws \Exception
+   */
+  public function __construct(TreeBuilder $tree_builder, Renderer $renderer) {
+
+
+    $this->treeBuilder = $tree_builder;
+    $this->renderer = $renderer;
+
+  }
+
+
 
   /**
    * {@inheritdoc}
@@ -29,6 +72,8 @@ class HeartbeatTypeForm extends EntityForm
     $heartbeat_type = $this->entity;
     $tokens = \Drupal::token()->getInfo();
     $form['#tree'] = TRUE;
+
+    $form['#attached']['library'] = 'heartbeat8/treeTable';
 
     $form['label'] = array(
       '#type' => 'textfield',
@@ -67,14 +112,6 @@ class HeartbeatTypeForm extends EntityForm
       '#default_value' => "Message",
       '#description' => $this->t("The structure for messages of this type. Use !exclamation marks before fields and entities"),
       '#required' => TRUE,
-//      '#ajax' => [
-//        'callback' => '::rebuildMessageArguments',
-//        'event' => 'change',
-//        'progress' => array(
-//          'type' => 'throbber',
-//          'message' => t('Rebuilding arguments'),
-//        ),
-//      ]
     );
 
 
@@ -139,6 +176,14 @@ class HeartbeatTypeForm extends EntityForm
           '#type' => 'textfield',
           '#title' => t($messageArguments[$i]),
           '#description' => t('Map value to this variable'),
+          '#ajax' => [
+            'callback' => '::tokenSelectDialog',
+            'event' => 'focus',
+            'progress' => array(
+              'type' => 'throbber',
+              'message' => t('Rebuilding arguments'),
+            ),
+          ]
         );
 
       }
@@ -154,6 +199,17 @@ class HeartbeatTypeForm extends EntityForm
       ],
     ];
 
+    if ($this->tokenTree == null) $this->tokenTree = $this->renderer->render($this->treeBuilder->buildAllRenderable([
+      'click_insert' => TRUE,
+      'show_restricted' => TRUE,
+      'show_nested' => FALSE,
+    ]));
+
+    $form['tokens'] = array(
+      '#prefix' => '<div id="token-tree"></div>',
+      '#markup' => $this->tokenTree
+    );
+
     $form['id'] = [
       '#type' => 'machine_name',
       '#default_value' => $heartbeat_type->id(),
@@ -166,112 +222,112 @@ class HeartbeatTypeForm extends EntityForm
 
     //Build temporary token form for developmental assistance
     $z = 0;
-    foreach ($tokens['tokens'] as $key => $type) {
-      if (is_array($type)) {
-        if (!is_array(current($type))) {
-
-          $form[$key] = array(
-            '#type' => 'details',
-            '#title' => t((string)strtoupper($key)),
-            '#collapsible' => TRUE,
-            '#collapsed' => TRUE,
-            '#states' => array(
-              'expanded' => array(
-                ':input[name="'.$key.'"]' => array('value' => 'expand'),
-              ),
-            ),
-          );
-          $s = 0;
-          foreach ($type as $token) {
-            if (!is_array($token)) {
-
-              $form[$key][$token->title] = array(
-                '#type' => 'item',
-                '#markup' => t((string)$token->title),
-                '#attributes' => array('tabindex' => 20+$z)
-              );
-            } else {
-              foreach ($token as $tkey => $subtoken) {
-
-                $form[$tkey][is_array($subtoken) ? key($subtoken) : $subtoken] = array(
-                  '#type' => 'details',
-                  '#title' => t('token'),
-                  '#collapsible' => TRUE,
-                  '#collapsed' => TRUE,
-                  '#states' => array(
-                    ':input[name="'.is_array($subtoken) ? key($subtoken) : $subtoken.'"]' => array('value' => 'expand2'),
-                  ));
-              }
-            }
-            $s++;
-          }
-          ksort($form[$key]);
-        } else {
-          $form[$key] = array(
-            '#type' => 'details',
-            '#title' => t((string)strtoupper($key)),
-            '#collapsible' => TRUE,
-            '#collapsed' => TRUE,
-            '#states' => array(
-              'expanded' => array(
-                ':input[name="'.$key.'"]' => array('value' => 'expand'),
-              ),
-            ),
-          );
-          foreach ($type as $skey => $subType) {
-            if (is_array($subType)) {
-              $form[$key][$skey] = array(
-                '#type' => 'details',
-                '#title' => t((string)$skey),
-                '#collapsible' => TRUE,
-                '#collapsed' => TRUE,
-                '#states' => array(
-                  'expanded' => array(
-                    ':input[name="'.$skey.'"]' => array('value' => 'expand'),
-                  ),
-                ),
-              );
-              foreach ($subType as $vskey => $token) {
-                if (!is_array($token)) {
-                  $form[$key][$skey][$vskey] = array(
-                    '#type' => 'item',
-//                                            '#title' => t(is_array($token) ? $vskey : $token),
-                    '#markup' => t(is_string($token) ? $token : is_string($vskey) ? $vskey : $key),
-                    '#attributes' => array('tabindex' => 20+$z)
-                  );
-                } else {
-                  $form[$key][$skey][$vskey] = array(
-                    '#type' => 'details',
-                    '#title' => $vskey,
-                    '#collapsible' => TRUE,
-                    '#collapsed' => TRUE,
-                    '#states' => array(
-                      'expanded' => array(
-                        ':input[name="'.$vskey.'"]' => array('value' => 'expand'),
-                      ),
-                    ),
-                  );
-                  foreach ($token as $subKey => $subtoken) {
-                    $form[$key][$skey][$vskey][is_array($subtoken->title) ? $subKey : $subtoken->title] = array(
-                      '#type' => 'item',
-                      '#markup' => t((string)is_array($subtoken) ? $subKey : $subtoken->title),
-                    );
-                  }
-                }
-              }
-              ksort($form[$key][$skey]);
-            }
-          }
-        }
-      } else {
-        $form[$key][$token == null ? 'null' : $token] = array(
-          '#type' => 'details',
-          '#title' => t($token == null ? 'null' : (string)$token),
-          '#markup' => t($token == null ? 'null' : (string)$token),
-        );
-      }
-      $z++;
-    }
+//    foreach ($tokens['tokens'] as $key => $type) {
+//      if (is_array($type)) {
+//        if (!is_array(current($type))) {
+//
+//          $form[$key] = array(
+//            '#type' => 'details',
+//            '#title' => t((string)strtoupper($key)),
+//            '#collapsible' => TRUE,
+//            '#collapsed' => TRUE,
+//            '#states' => array(
+//              'expanded' => array(
+//                ':input[name="'.$key.'"]' => array('value' => 'expand'),
+//              ),
+//            ),
+//          );
+//          $s = 0;
+//          foreach ($type as $token) {
+//            if (!is_array($token)) {
+//
+//              $form[$key][$token->title] = array(
+//                '#type' => 'item',
+//                '#markup' => t((string)$token->title),
+//                '#attributes' => array('tabindex' => 20+$z)
+//              );
+//            } else {
+//              foreach ($token as $tkey => $subtoken) {
+//
+//                $form[$tkey][is_array($subtoken) ? key($subtoken) : $subtoken] = array(
+//                  '#type' => 'details',
+//                  '#title' => t('token'),
+//                  '#collapsible' => TRUE,
+//                  '#collapsed' => TRUE,
+//                  '#states' => array(
+//                    ':input[name="'.is_array($subtoken) ? key($subtoken) : $subtoken.'"]' => array('value' => 'expand2'),
+//                  ));
+//              }
+//            }
+//            $s++;
+//          }
+//          ksort($form[$key]);
+//        } else {
+//          $form[$key] = array(
+//            '#type' => 'details',
+//            '#title' => t((string)strtoupper($key)),
+//            '#collapsible' => TRUE,
+//            '#collapsed' => TRUE,
+//            '#states' => array(
+//              'expanded' => array(
+//                ':input[name="'.$key.'"]' => array('value' => 'expand'),
+//              ),
+//            ),
+//          );
+//          foreach ($type as $skey => $subType) {
+//            if (is_array($subType)) {
+//              $form[$key][$skey] = array(
+//                '#type' => 'details',
+//                '#title' => t((string)$skey),
+//                '#collapsible' => TRUE,
+//                '#collapsed' => TRUE,
+//                '#states' => array(
+//                  'expanded' => array(
+//                    ':input[name="'.$skey.'"]' => array('value' => 'expand'),
+//                  ),
+//                ),
+//              );
+//              foreach ($subType as $vskey => $token) {
+//                if (!is_array($token)) {
+//                  $form[$key][$skey][$vskey] = array(
+//                    '#type' => 'item',
+////                                            '#title' => t(is_array($token) ? $vskey : $token),
+//                    '#markup' => t(is_string($token) ? $token : is_string($vskey) ? $vskey : $key),
+//                    '#attributes' => array('tabindex' => 20+$z)
+//                  );
+//                } else {
+//                  $form[$key][$skey][$vskey] = array(
+//                    '#type' => 'details',
+//                    '#title' => $vskey,
+//                    '#collapsible' => TRUE,
+//                    '#collapsed' => TRUE,
+//                    '#states' => array(
+//                      'expanded' => array(
+//                        ':input[name="'.$vskey.'"]' => array('value' => 'expand'),
+//                      ),
+//                    ),
+//                  );
+//                  foreach ($token as $subKey => $subtoken) {
+//                    $form[$key][$skey][$vskey][is_array($subtoken->title) ? $subKey : $subtoken->title] = array(
+//                      '#type' => 'item',
+//                      '#markup' => t((string)is_array($subtoken) ? $subKey : $subtoken->title),
+//                    );
+//                  }
+//                }
+//              }
+//              ksort($form[$key][$skey]);
+//            }
+//          }
+//        }
+//      } else {
+//        $form[$key][$token == null ? 'null' : $token] = array(
+//          '#type' => 'details',
+//          '#title' => t($token == null ? 'null' : (string)$token),
+//          '#markup' => t($token == null ? 'null' : (string)$token),
+//        );
+//      }
+//      $z++;
+//    }
 
     $form_state->setCached(FALSE);
 
@@ -334,5 +390,25 @@ class HeartbeatTypeForm extends EntityForm
     return $form['variables'];
   }
 
+
+  public function tokenSelectDialog(array &$form, FormStateInterface $form_state) {
+    // Instantiate an AjaxResponse Object to return.
+    $ajax_response = new AjaxResponse();
+
+
+    // Add a command to execute on form, jQuery .html() replaces content between tags.
+    // In this case, we replace the description with whether the username was found or not.
+//    $ajax_response->addCommand(new HtmlCommand('#token-tree', $output));
+
+    // CssCommand did not work.
+    //$ajax_response->addCommand(new CssCommand('#edit-user-name--description', array('color', $color)));
+//    $color = 'pink';
+    // Add a command, InvokeCommand, which allows for custom jQuery commands.
+    // In this case, we alter the color of the description.
+    $ajax_response->addCommand(new InvokeCommand('#token-tree', 'css', array('display', 'block')));
+
+    // Return the AjaxResponse Object.
+    return $ajax_response;
+  }
 
 }
