9fe1cafb7a5d4e5b8dc77e7fe7d062581fc3d975
logicp <emmanuel@openconcept.ca>
2017-06-25 04:01:27 -0400

    adding flag javascript

--- themes/kekistan/js/kek.js
+++ themes/kekistan/js/kek.js
@@ -5,7 +5,7 @@
       let feedFilterBlock = document.getElementById('block-views-feed-filter-block');
       let terms = feedFilterBlock.querySelectorAll('a');
 
-      terms.forEach(function(term) {
+      terms.forEach(function (term) {
         console.dir(term);
         let tid = term.href.substring(term.href.lastIndexOf('/') + 1);
         term.addEventListener('click', function (event) {
@@ -43,7 +43,7 @@
       // console.dir(videos);
       for (let i = videos.length - 1; i > -1; i--) {
         let video = videos[i];
-        video.addEventListener('loadedmetadata', function() {
+        video.addEventListener('loadedmetadata', function () {
           video.loop = video.duration < 5;
 
           window.addEventListener('scroll', checkScroll, false);
@@ -56,7 +56,7 @@
 
         console.log('scroll');
 
-        for(var i = 0; i < videos.length; i++) {
+        for (var i = 0; i < videos.length; i++) {
 
           var video = videos[i];
 
@@ -78,9 +78,50 @@
         }
 
       }
+      displayCounts();
     }
   };
 
+
+  function displayCounts() {
+    var likefelFlags = document.querySelectorAll('.flag-heartbeat_like');
+    var jihadFlags = document.querySelectorAll('.flag-jihad_flag');
+    for (let i = 0; i < likefelFlags.length; i++) {
+
+      console.dir(likefelFlags[i]);
+      for (let g = 0; g < likefelFlags[i].childNodes.length; g++) {
+        if (likefelFlags[i].childNodes[g].tagName === 'A') {
+          let anchor = likefelFlags[i].childNodes[g];
+          let countSpan = document.createElement('span');
+          // if (anchor.nextSibling !== null && anchor.nextSibling.tagName !== 'span') {
+
+            countSpan.innerText = anchor.innerText.substring(anchor.innerText.lastIndexOf("(") + 1, anchor.innerText.lastIndexOf(")"));
+            console.log(countSpan.innerText);
+            anchor.after(countSpan);
+          // }
+        }
+      }
+    }
+    for (let i = 0; i < jihadFlags.length; i++) {
+
+      console.dir(jihadFlags[i]);
+      for (let g = 0; g < jihadFlags[i].childNodes.length; g++) {
+        if (jihadFlags[i].childNodes[g].tagName === 'A') {
+          let anchor = jihadFlags[i].childNodes[g];
+          let countSpan = document.createElement('span');
+
+
+          // if (anchor.nextSibling !== null && anchor.nextSibling.tagName !== 'span') {
+
+
+            countSpan.innerText = anchor.innerText.substring(anchor.innerText.lastIndexOf("(") + 1, anchor.innerText.lastIndexOf(")"));
+            console.log(countSpan.innerText);
+            anchor.after(countSpan);
+          // }
+        }
+      }
+    }
+  }
 
 })(jQuery, Drupal, drupalSettings);
 
