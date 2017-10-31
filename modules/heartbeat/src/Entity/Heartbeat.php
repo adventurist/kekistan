63cf519a7ced4389db2d22ffc6ecd34b2ef35d2a
logicp <emmanuel@openconcept.ca>
2017-10-31 11:03:05 -0400

    adding fullscreen support for youtube videos

--- modules/heartbeat/src/Entity/Heartbeat.php
+++ modules/heartbeat/src/Entity/Heartbeat.php
@@ -543,7 +543,7 @@ class Heartbeat extends RevisionableContentEntityBase implements HeartbeatInterf
       return '<' . $type . ' src="' . str_replace('public://', '/sites/default/files/', $filePath) . '" class="heartbeat-image" / >';
     } else if ($type == 'youtube') {
       $filePath = str_replace('youtube://', 'https://www.youtube.com/embed/', $filePath);
-      return '<iframe class="heartbeat-youtube" width="auto" height="auto" src="' . $filePath . '" frameborder="0"></iframe>';
+      return '<iframe class="heartbeat-youtube" width="auto" height="auto" src="' . $filePath . '" frameborder="0" allowfullscreen="allowfullscreen"></iframe>';
     } else if ($type == 'video') {
       return '<' . $type . ' controls src="' . str_replace('public://', '/sites/default/files/', $filePath) . '" class="heartbeat-video"></' . $type . '>';
     }
